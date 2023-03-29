bf45f1292690f9d6b9563a5b9ce07cde3e4e9720
Mahmood <mahmoodrajmohamed@gmail.com>
2023-03-29 22:10:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   return (
+    <div className="App">
       <header>
         <form id="to-do-form">
           <input type="text" placeholder="Enter name"></input>
           <button type="submit">Add</button>
         </form>
       </header>
+      </div>
   );
 }
 
